050f5cbf7086207f58524529ecdf9b681ee069de
Eduards Usačovs <usacovs.eduards@gmail.com>
2022-03-14 15:31:15 +0200

    Manage Admin Page Design

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,8 +4,52 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Manage Admin</h1>
-
             
+            <br>
+            <br>
+
+            <!-- Button to Add Admin -->
+            <a href="#" class="btn-primary">Add Admin</a>
+
+            <br>
+            <br>
+            <br>
+
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N.</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1. </td>
+                    <td>Eduards Usačovs</td>
+                    <td>eduardsusacovs</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2. </td>
+                    <td>Eduards Usačovs</td>
+                    <td>eduardsusacovs</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3. </td>
+                    <td>Eduards Usačovs</td>
+                    <td>eduardsusacovs</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
     <!-- Main Content Section Ends -->
--- admin/manage-category.php
+++ admin/manage-category.php
@@ -1,9 +1,55 @@
 <?php include('partials/menu.php'); ?>
 
-<div class="main-content">
-    <div class="wrapper">
-        <h1>Manage Category</h1>
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Category</h1>
+
+            <br>
+            <br>
+
+            <!-- Button to Add Category -->
+            <a href="#" class="btn-primary">Add Category</a>
+
+            <br>
+            <br>
+            <br>
+
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N.</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1. </td>
+                    <td>Eduards Usačovs</td>
+                    <td>eduardsusacovs</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Category</a>
+                        <a href="#" class="btn-danger">Delete Category</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2. </td>
+                    <td>Eduards Usačovs</td>
+                    <td>eduardsusacovs</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Category</a>
+                        <a href="#" class="btn-danger">Delete Category</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3. </td>
+                    <td>Eduards Usačovs</td>
+                    <td>eduardsusacovs</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Category</a>
+                        <a href="#" class="btn-danger">Delete Category</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
-</div>
 
 <?php include('partials/footer.php') ?>

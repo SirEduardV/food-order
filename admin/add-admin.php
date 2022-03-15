e34742129587c7d08d34e5be9e50224102bec3e1
Eduards Usačovs <usacovs.eduards@gmail.com>
2022-03-15 11:44:10 +0200

    Add Admin to Database

---
+++ admin/add-admin.php
@@ -0,0 +1,67 @@
+<?php include('partials/menu.php'); ?>
+
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Add Admin</h1>
+
+            <br>
+            <br>
+
+            <form action="" method="POST">
+                <table class="tbl-30">
+                    <tr>
+                        <td>Full Name: </td>
+                        <td>
+                            <input type="text" name="full_name" placeholder="Enter Your Name">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Username: </td>
+                        <td>
+                            <input type="text" name="username" placeholder="Your Username">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Password: </td>
+                        <td>
+                            <input type="password" name="password" placeholder="Your Password">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+
+<?php include('partials/footer.php'); ?>
+
+<?php
+    // Process the Value from Form and Save it in Database
+    // 1. Check whether the Button is clicked or not
+
+    if(isset($_POST['submit'])) {
+        // Button clicked
+        // echo "Clicked";
+        // Get the Data from Form
+        $full_name = $_POST['full_name'];
+        $username = $_POST['username'];
+        $password = md5($_POST['password']); // Password Encryption with MD5
+
+        // 2. SQL Query to Save the data into database
+        $sql = "INSERT INTO tbl_admin SET
+            full_name = '$full_name',
+            username = '$username',
+            password = '$password'
+        ";
+
+        // 3. Execute Query and Save Data in Database
+        $conn = mysqli_connect('food-order', 'mysql', '') or die(mysqli_error($e)); // Database Connection
+        $db_select = mysqli_select_db($conn, 'food-order') or die(mysqli_error($e)); // Selecting Database
+
+        // $res = mysqli_query($conn, $sql) or die(mysqli_error($e));
+    }
+?>
--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,12 +4,12 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Manage Admin</h1>
-            
+
             <br>
             <br>
 
             <!-- Button to Add Admin -->
-            <a href="#" class="btn-primary">Add Admin</a>
+            <a href="add-admin.php" class="btn-primary">Add Admin</a>
 
             <br>
             <br>

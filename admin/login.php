06df95a0ae1f7384659c7b02d3897839af70b1d1
Eduards Usačovs <usacovs.eduards@gmail.com>
2022-03-17 16:49:13 +0200

    Login Page Design

---
+++ admin/login.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>Login - Food Order System</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <div class="login">
+            <h1 class="text-center">Login</h1>
+
+            <br>
+            <br>
+
+            <!-- Login Form Starts Here -->
+            <form action="" method="POST" class="text-center">
+                Username
+                <br>
+                <input type="text" name="username" placeholder="Enter Username"> 
+                <br>
+                <br>
+                Password
+                <br>
+                <input type="password" name="password" placeholder="Enter Password"> 
+                <br>
+                <br>
+                <input type="submit" name="submit" value="Login" class="btn-primary">
+                <br>
+                <br>
+            </form>
+            <!-- Login Form Ends Here -->
+            <p class="text-center">Created By - <a href="#">Eduards Usačovs</a></p>
+        </div>
+    </body>
+</html>
